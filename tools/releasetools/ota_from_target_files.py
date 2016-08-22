e3595d83e783c8a65f4cb6e13a3346b8753b283e
NetFluke <netfluke@yahoo.com>
2016-08-22 19:27:43 -0400

    Remove SetupWizard on dirty flash
    Deleting GAPPS SetupWizard on dirty flash to avoid issues

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -733,6 +733,11 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
+  script.Mount("/system")
+  script.Print("Some cleanup...")
+  script.DeleteRecursive("/system/priv-app/SetupWizard")
+  script.Unmount("/system")
+
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
 
